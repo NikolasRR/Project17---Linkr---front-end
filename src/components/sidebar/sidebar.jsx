4560f40ee43055c1a6f6fbee6893fb411825d863
Victor Matos <antoniovitomatos@gmail.com>
2022-06-15 13:59:37 -0300

    side bar comunicação com o back

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,15 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
 import axios from "axios";
 
 import {SideBar} from './style';
+
 export default function Trending(){
-
-
+    const [trending, setTrending] = useState();
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const {data} = await axios.get('http://localhost:5000/trending');
+                setTrending(data);
+            } catch (error) {
+                console.log(error.response);
+            }
+        }
+        getData();
+    }, []);
     return(
         <SideBar>
             <h1>trending</h1>
-            <p># exemple</p>
-            <p># exemple</p>
-            <p># exemple</p>
+            <ul>
+                {trending && trending.map((item, index) => {
+                    return(
+                        <li key={index}>
+                            <Link to={`/hashtag/${item.content}`}>
+                            <p>#{item.content}</p>
+                            </Link>
+                        </li>
+                    )
+                }
+                )}
+            </ul>
         </SideBar>
     );
 }
