1e37392729439cc409a9220ae503522659f76a06
Nikolas <nkls99@gmail.com>
2022-06-15 20:10:42 -0300

    UserData context reseter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
+import UserContext from "../contexts/UserContext";
 
 function Header () {
     const navigate = useNavigate();
+    const { setUserData } = useContext(UserContext);
 
     useEffect(() => {
         async function logout () {
             try {
                 await axios.get("http://localhost:5000/logout", { withCredentials: true });
+                setUserData({});
                 navigate("/");
             } catch (error) {
                 alert('Something went wrong');
