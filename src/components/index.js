3f357509b5ba14cf697b305063da8f2f5bd1e6f0
Nikolas <nkls99@gmail.com>
2022-06-15 18:44:56 -0300

    persistent login

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
+import axios from "axios";
+
+import UserContext from "../contexts/UserContext";
 
 import SignInScreen from "./signInScreen";
 
 function App() {
-    
+    const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        async function getUser () {
+            const user = await axios.get("http://localhost:5000/session", { withCredentials: true });
+            setUserData(user);
+        }
+        getUser();
+    }, []);
+
+    const contextValue = { userData, setUserData };
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<SignInScreen />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <UserContext.Provider value={contextValue}>
+                    <Routes>
+                        <Route path="/" element={<SignInScreen />} />
+                    </Routes>
+                </UserContext.Provider>
+            </BrowserRouter>
+        </>
     )
 }
 
