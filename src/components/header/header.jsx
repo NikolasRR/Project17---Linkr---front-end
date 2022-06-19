e085285cd67b706fc6d25ea1aaa28c7801d3d9c3
Victor Matos <antoniovitomatos@gmail.com>
2022-06-19 19:12:07 -0300

    search user

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,23 +3,36 @@ import { IoSearchSharp } from "react-icons/io5";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
+import Result from "./../searchUser/searchUser";
+import  { DebounceInput }  from  'react-debounce-input' ;
 
 import UserContext from "../../contexts/UserContext";
 
-import { Main, Logo, Middle, Input, Rigth, ProfileImage, UserOptions, LogoutOption } from "./../header/style"
+
+import { Main, Logo, Middle, Input, Rigth, ProfileImage, UserOptions, LogoutOption, ResultStyle, Content } from "./../header/style"
 
 function Header() {
     const navigate = useNavigate();
     const [menuOpen, setMenuOpen] = useState(false);
     const {userData,setUserData } = useContext(UserContext);
-
+    const [searchData, setSearchData] = useState([]);
     function goToTimeLine(){
         navigate(`/`)
     }
     
-    function search(e){
+    async function search(e){
         e.preventDefault()
-        
+        const { value } = e.target;
+        if(value.length > 2){
+        try{
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/search/${value}`, { withCredentials: true });
+            const {data} = response;
+            setSearchData(data)
+        }catch(error){
+            console.error(error);
+        }
+        }
+        else{setSearchData([]);}
     }
 
     async function logout () {
@@ -37,12 +50,20 @@ function Header() {
     return (
         <Main>
             <Logo onClick={()=> goToTimeLine()} >Linkr</Logo>
+            <Content>
             <Middle>
-            <form onSubmit={search}>
-                <Input placeholder="Search for people"></Input>
-            </form>
+            
+                <DebounceInput debounceTimeout = { 300 } placeholder="Search for people" onChange={search}/>
+            
+            
                 <div><IoSearchSharp></IoSearchSharp></div>
             </Middle>
+            <ResultStyle>
+                {searchData.length > 0 && searchData.map((data, index) => 
+                <Result key={index} value={data}></Result>) 
+                }
+            </ResultStyle>
+            </Content>
             <Rigth onClick={() => setMenuOpen(!menuOpen)}>
                 <div>{menuOpen ? <IoIosArrowUp></IoIosArrowUp> : <IoIosArrowDown></IoIosArrowDown>}</div>
                 <ProfileImage src={userData.image}></ProfileImage>
---
+++ src/components/searchUser/searchUser.jsx
@@ -0,0 +1,10 @@
+import {Main} from "./style";
+export default function Result(props){
+    const { value } = props;
+    return(
+        <Main>
+            <img src={value.image} alt ={value.userName} />
+            <p>{value.userName}</p>
+        </Main>
+    )
+}
